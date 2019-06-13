d14cb22f7f77280a0aa9aedbdd32db24df9b89f3
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-06-12 20:10:32 -0500

    added routes the sequelized way

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,1 +1,28 @@
 var db = require("../models");
+
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+        db.Taco.findAll({}).then(function(data) {
+            res.render("index", {taco_data: data});
+          });
+    });
+    app.post("/", function(req, res) {
+        db.Taco.create({
+          taco_name: req.body.name
+        }).then(function(results) {
+          res.redirect("/");
+        });    
+    });
+    app.put("/:id", function(req, res) {
+        db.Taco.update({
+          picked_up:req.body.picked_up
+        }, 
+        {
+            where: {id: req.params.id}
+        }).then(function(results) {
+          res.redirect("/");
+        });
+    });
+
+}
+
